c902590b2a55f9254ccbb086344b84f1a80fa426
Jocelyn Liu <yrliou@gmail.com>
2021-03-01 16:20:45 -0800

    Remove patch of stub_resolver_config_reader.cc and settings_secure_dns_handler.cc

--- chromium_src/chrome/browser/net/stub_resolver_config_reader.cc
+++ chromium_src/chrome/browser/net/stub_resolver_config_reader.cc
@@ -4,6 +4,7 @@
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "brave/components/unstoppable_domains/buildflags/buildflags.h"
+#include "chrome/browser/net/secure_dns_util.h"
 #include "components/prefs/pref_service.h"
 
 #if BUILDFLAG(UNSTOPPABLE_DOMAINS_ENABLED)
@@ -12,35 +13,36 @@
 #include "brave/net/unstoppable_domains/constants.h"
 #endif
 
-namespace {
-
-#if BUILDFLAG(UNSTOPPABLE_DOMAINS_ENABLED)
-void AddUnstoppableDomainsResolver(std::string* doh_templates,
-                                   PrefService* local_state) {
-  if (unstoppable_domains::IsResolveMethodDoH(local_state) &&
-      doh_templates->find(unstoppable_domains::kDoHResolver) ==
-          std::string::npos) {
-    *doh_templates =
-        base::StrCat({unstoppable_domains::kDoHResolver, " ", *doh_templates});
-  }
-}
-#endif  // BUILDFLAG(UNSTOPPABLE_DOMAINS_ENABLED)
+namespace chrome_browser_net {
+namespace secure_dns {
 
 // Add DoH servers to support Unstoppable Domains (and ENS in the future PR).
 // These servers are controlled using its own prefs and not
 // kDnsOverHttpsTemplates pref as the servers we added here are special and
 // only applies to certain TLD which is different from user's global DoH
 // provider settings.
-void AddDoHServers(std::string* doh_templates, PrefService* local_state) {
+std::vector<base::StringPiece> SplitGroup(
+    std::string& group,
+    PrefService* local_state,
+    bool force_check_parental_controls_for_automatic_mode) {
 #if BUILDFLAG(UNSTOPPABLE_DOMAINS_ENABLED)
-  AddUnstoppableDomainsResolver(doh_templates, local_state);
+  // force_check_parental_controls_for_automatic_mode is only true for
+  // settings UI which we specifically do not want to display those special
+  // resolvers we added.
+  if (!force_check_parental_controls_for_automatic_mode &&
+      unstoppable_domains::IsResolveMethodDoH(local_state) &&
+      group.find(unstoppable_domains::kDoHResolver) == std::string::npos) {
+    group = base::StrCat({unstoppable_domains::kDoHResolver, " ", group});
+  }
 #endif
+
+  return chrome_browser_net::secure_dns::SplitGroup(group);
 }
 
-}  // namespace
+}  // namespace secure_dns
+}  // namespace chrome_browser_net
 
-#define BRAVE_GET_AND_UPDATE_CONFIGURATION \
-  AddDoHServers(&doh_templates, local_state_);
-
+#define SplitGroup(doh_templates)         \
+  SplitGroup(doh_templates, local_state_, \
+             force_check_parental_controls_for_automatic_mode)
 #include "../../../../../chrome/browser/net/stub_resolver_config_reader.cc"
-#undef BRAVE_GET_AND_UPDATE_CONFIGURATION
